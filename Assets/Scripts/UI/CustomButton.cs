a26df424266a9cdb080b7a6cb0dee24ab9a481cb
TaitotaloSanteri <santeri.hakoniemi@edu.taitotalo.fi>
2021-03-09 08:58:17 +0200

    Initial commit

---
+++ Assets/Scripts/UI/CustomButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CustomButton : MonoBehaviour, 
+                            IPointerDownHandler, 
+                            IPointerEnterHandler, 
+                            IPointerExitHandler, 
+                            IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Hiiri tuotu napin päälle.");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    }
+}
